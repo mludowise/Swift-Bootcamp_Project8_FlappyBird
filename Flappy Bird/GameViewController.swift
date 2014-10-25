75485f6f5d4bc62a9d996286504abce8570e9a5b
mludowise <mludowise@gmail.com>
2014-10-24 19:40:51 -0700

    Pushing Bird

--- Flappy Bird/GameViewController.swift
+++ Flappy Bird/GameViewController.swift
@@ -8,25 +8,42 @@
 
 import UIKit
 
+private let kTouchVelocity = CGPoint(x: 0.0, y: -425.0)
+
 class GameViewController: UIViewController {
     
     @IBOutlet weak var bird: UIImageView!
     
     var animator : UIDynamicAnimator!
-    var gravity = UIGravityBehavior()
+    var gravity : UIGravityBehavior!
     var birdItemBehavior : UIDynamicItemBehavior!
+    var collisionBehavior : UICollisionBehavior!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         animator = UIDynamicAnimator(referenceView: view)
+        
+        gravity = UIGravityBehavior(items: [bird])
+        gravity.magnitude = 1.0
         animator.addBehavior(gravity)
-        gravity.addItem(bird)
+        
+        birdItemBehavior = UIDynamicItemBehavior(items: [bird])
+        birdItemBehavior.allowsRotation = false
+        animator.addBehavior(birdItemBehavior)
+
+//        collisionBehavior = UICollisionBehavior(items: [bird])
+//        collisionBehavior.delete(self)
+//        animator.addBehavior(collisionBehavior)
         
         
     }
     
     @IBAction func onTap(sender: UITapGestureRecognizer) {
-        
+        var velocity = birdItemBehavior.linearVelocityForItem(bird)
+        velocity.x = 0
+        velocity.y = kTouchVelocity.y - velocity.y
+        birdItemBehavior.addLinearVelocity(velocity, forItem: bird)
     }
     
 }
