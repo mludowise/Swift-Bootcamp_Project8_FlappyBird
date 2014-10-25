854871630472f10b4ec17484e1aedfb9f88aa9e6
mludowise <mludowise@gmail.com>
2014-10-25 01:20:03 -0700

    Pipes move

--- Flappy Bird/GameViewController.swift
+++ Flappy Bird/GameViewController.swift
@@ -8,19 +8,30 @@
 
 import UIKit
 
-private let kTouchVelocity = CGPoint(x: 0.0, y: -425.0)
+private let kTouchVelocity = CGPoint(x: 0, y: -320)
+private let kPipeDistance = 100
+private let kPipeSpacing = 100
+private let kPipeVelocity = CGPoint(x: -100, y: 0)
 
 class GameViewController: UIViewController {
     
     @IBOutlet weak var bird: UIImageView!
+    @IBOutlet weak var topPipe: UIImageView!
+    @IBOutlet weak var bottomPipe: UIImageView!
     
     var animator : UIDynamicAnimator!
     var gravity : UIGravityBehavior!
     var birdItemBehavior : UIDynamicItemBehavior!
-    var collisionBehavior : UICollisionBehavior!
+//    var collisionBehavior : UICollisionBehavior!
+    var pipeItemBehavior : UIDynamicItemBehavior!
+    
+    var pipes : [UIView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pipes.append(topPipe)
+        pipes.append(bottomPipe)
         
         animator = UIDynamicAnimator(referenceView: view)
         
@@ -30,13 +41,17 @@ class GameViewController: UIViewController {
         
         birdItemBehavior = UIDynamicItemBehavior(items: [bird])
         birdItemBehavior.allowsRotation = false
+        birdItemBehavior.addLinearVelocity(kTouchVelocity, forItem: bird)
         animator.addBehavior(birdItemBehavior)
 
 //        collisionBehavior = UICollisionBehavior(items: [bird])
 //        collisionBehavior.delete(self)
 //        animator.addBehavior(collisionBehavior)
         
-        
+        pipeItemBehavior = UIDynamicItemBehavior(items: pipes)
+        pipeItemBehavior.addLinearVelocity(kPipeVelocity, forItem: topPipe)
+        pipeItemBehavior.addLinearVelocity(kPipeVelocity, forItem: bottomPipe)
+        animator.addBehavior(pipeItemBehavior)
     }
     
     @IBAction func onTap(sender: UITapGestureRecognizer) {
